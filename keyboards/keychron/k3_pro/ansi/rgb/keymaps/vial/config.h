86dc68ed0e1e5dbd834dfff6daccfd3b85d96121
Vagelis Panos <vagelisp.97@gmail.com>
2023-12-09 02:32:56 +0200

    k3_pro default firmware ported

--- keyboards/keychron/k3_pro/ansi/rgb/keymaps/vial/config.h
+++ keyboards/keychron/k3_pro/ansi/rgb/keymaps/vial/config.h
@@ -2,11 +2,7 @@
 
 #pragma once
 
-#define VIAL_KEYBOARD_UID \
-    { 0xDB, 0xCF, 0xB8, 0x5C, 0xCC, 0x1A, 0x74, 0x7B }
+#define VIAL_KEYBOARD_UID {0x9D, 0xEE, 0xF0, 0xB7, 0xAC, 0xED, 0x8B, 0xF2}
 
-#define VIAL_UNLOCK_COMBO_ROWS \
-    { 0, 3 }
-
-#define VIAL_UNLOCK_COMBO_COLS \
-    { 0, 13 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
